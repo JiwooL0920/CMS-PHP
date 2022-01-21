4cde8f83fce62db0133e7dae958963d431deb5ec
jiwool0920 <jiwool0920@gmail.com>
2022-01-21 12:41:44 -0500

    project tree setup

---
+++ public/staff/index.php
@@ -0,0 +1,23 @@
+
+<html lang="en">
+    <head>
+        <title>GBI</title>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <header>
+            <h1>GBI Staff Area</h1>
+        </header>
+    </body>
+
+    <navigation>
+        <ul>
+            <li><a href="index.php"></a></li>
+        </ul>
+    </navigation>
+
+    <footer>
+        &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
+</html>
